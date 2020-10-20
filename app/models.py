8126fb62b420d50fd4fa40992fda33c476e04774
tiago <tyagosampaio@gmail.com>
2020-10-19 23:12:56 -0300

    last commint no save changes

--- app/models.py
+++ app/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
 # Create your models here.
+
+class DroneCategory(models.Model):
+    name = models.CharField(max_length=250)
+
+    #order by name
+    class Meta:
+        ordering = ("name",)
+    
+    class __str__(self):
+        return self.name
+    
+class Drone(models.Model):
+    name = models.CharField(max_length=250, blank=False, null=False)
+    dronecategory = models.ForeignKey('DroneCategory',
+                                       related_name='drones',
+                                       on_delete=models.CASCADE)
+    manufacturing_date = models.DateTimeField()
+    has_it_competed = models.BooleanField(defaul=False)
+    inserted_timestamp = models.DateTime(auto_now_add=True)
+
+    class Meta:
+        ordering = ("name",)
+
+    class __str__(self):
+        return self.name
+
+class Pilot(models.Model):
+    MALE = 'M'
+    FAMELE = 'F'
+    GENDER_CHOICES = (
+        (MALE,'Male'),
+        (FAMELE,'Famale'),
+    )
--- drones/settings.py
+++ drones/settings.py
@@ -28,7 +28,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'toy.apps.ToyConfig',
+    #'toy.apps.ToyConfig',
+    'app.apps.AppConfig',       
 
 ]
 
