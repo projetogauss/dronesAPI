510bc4761c3e76cbbd435cd3c1369329721609d5
Tiago Sampaio <tyagosampaio@gmail.com>
2020-10-21 02:14:25 -0300

    finish serializer file

--- app/serializers.py
+++ app/serializers.py
@@ -18,3 +18,59 @@ class DroneCategorySerializer(serializers.HyperlinkedModelSerializer):
 class DroneSerializer(serializers.HyperlinkedModelSerializer):
     drone_category = serializers.SlugRelatedField(queryset=DroneCategory.objects.all(), 
                                                   sluge_field='name')
+    class Meta:
+        model = Drone
+        fields = (
+            'url',
+            'name',
+            'drone_category',
+            'manufacturing_date',
+            'has_it_competed',
+            'inserted_timestamp'
+        )
+
+class CompetitionSerializer(serializers.HyperlinkedModelSerializer):
+    drone = DroneSerializer()
+
+    class Meta:
+        model = Competition
+        fields = (
+            'url',
+            'pk',
+            'distance_in_feet',
+            'distance_achievement_date',
+            'drone'
+        )
+
+class PilotSerializer(serializers.HyperlinkedModelSerializer):
+    competition = CompetitionSerializer(many=True, read_only=True)
+    gender = serializers.ChoiceField(choices=Pilot.GENDER_CHOICES)
+    gender_description = serializers.CharField(source='get_gender_display', read_only=True)
+
+    class Meta:
+        model = Pilot
+        fields = (
+            'url',
+            'name',
+            'gender',
+            'gender_description',
+            'races_count',
+            'inserted_timestamp',
+            'competitions'
+        )
+
+class PilotCompetitionSerializer(serializers.ModelSerializer):
+ # Display the pilot's name
+    pilot = serializers.SlugRelatedField(queryset=Pilot.objects.all(),slug_field='name')
+ # Display the drone's name
+    drone = serializers.SlugRelatedField(queryset=Drone.objects.all(),slug_field='name')
+    class Meta:
+        model = Competition
+        fields = (
+            'url',
+            'pk',
+            'distance_in_feet',
+            'distance_achievement_date',
+            'pilot',
+            'drone')
+
